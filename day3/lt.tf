e7e2e4fd6b0aa18410b39ff749bccd513f8fbe48
srushtilohakare111-hue <srushtilohakre111@gmail.com>
2026-02-28 16:24:33 +0530

    update

--- day3/lt.tf
+++ day3/lt.tf
@@ -12,7 +12,7 @@ resource "aws_launch_template" "home_launch_template" {
     env = var.env
   }
   vpc_security_group_ids = [aws_security_group.security_group.id]
-  user_data = base64decode(<<-EOF
+  user_data = base64encode(<<-EOF
   #!/bin/bash
   apt update -y
   apt install apache2 -y
@@ -33,7 +33,7 @@ resource "aws_launch_template" "laptop_launch_template" {
     env = var.env
   }
   vpc_security_group_ids = [aws_security_group.security_group.id]
-  user_data = base64decode(<<-EOF
+  user_data = base64encode(<<-EOF
   #!/bin/bash
   apt update -y
   apt install apache2 -y
@@ -55,7 +55,7 @@ resource "aws_launch_template" "mobile_launch_template" {
     env = var.env
   }
   vpc_security_group_ids = [aws_security_group.security_group.id]
-  user_data = base64decode(<<-EOF
+  user_data = base64encode(<<-EOF
   #!/bin/bash
   apt update -y
   apt install apache2 -y
